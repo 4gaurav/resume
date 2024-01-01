55536e820a5738f958225bd6647f09017c70f287
4gaurav <107033627+4gaurav@users.noreply.github.com>
2024-01-01 14:18:01 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,60 @@
+var swiper = new Swiper('.mySwap', {
+    centeredSlides: true,
+    slidesPerView: 1.6,
+    loop: true,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+               delay: 2000,
+           },
+  });
+  
+  ///////////////////////////////////////////////////
+  
+  var swiper = new Swiper('.mySwap-2', {
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+               delay: 2000,
+           },
+  });
+
+
+  ///////////////////////////////////////////////////
+
+  var swiper = new Swiper('.mySwiper', {
+    centeredSlides: true,
+    slidesPerView: 4,
+    loop: true,
+    spaceBetween: 1,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+               delay: 2000,
+           },
+  });
+  
+  var swiper = new Swiper('.mySwiper-2', {
+    centeredSlides: true,
+    slidesPerView: 1.8,
+    loop: true,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+               delay: 2000,
+           },
+  });
